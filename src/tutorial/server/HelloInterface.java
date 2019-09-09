de1d3b5b8caf2baac2361f80c373356efce8610a
Abhinav Anand <abhinav_anand@live.com>
2019-09-09 23:10:35 +0530

    Add spec pdf

--- src/tutorial/server/HelloInterface.java
+++
@@ -1,9 +0,0 @@
-package tutorial.server;
-
-import java.rmi.Remote;
-import java.rmi.RemoteException;
-
-public interface HelloInterface extends Remote {
-    public String sayHello(String name)
-            throws RemoteException;
-}
